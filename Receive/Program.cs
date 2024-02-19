d31894ef6f9ead49e3249d4ef06d15031cdf91ee
Ronan Rocha <ronan.rochasouza@gmail.com>
2024-02-19 18:27:16 -0300

    termino do exemplo e comentarios pertinentes

--- Receive/Program.cs
+++ Receive/Program.cs
@@ -7,10 +7,18 @@ using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
 channel.QueueDeclare(queue: "hello",
-                     durable: false,
+                     durable: true, // marca a fila duravel não será apagada quando o rabbitmq for reiniciado
                      exclusive: false,
                      autoDelete: false,
                      arguments: null);
+
+
+/*
+ Isso diz ao RabbitMQ para não fornecer mais de uma mensagem a um trabalhador por vez.
+ Ou, em outras palavras, não envie uma nova mensagem para um trabalhador até que ele tenha processado e confirmado a anterior.
+ Em vez disso, ele irá despachá-lo para o próximo trabalhador que ainda não esteja ocupado.
+*/
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
 Console.WriteLine(" [*] Waiting for messages.");
 
--- Send/Program.cs
+++ Send/Program.cs
@@ -6,7 +6,7 @@ using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
 channel.QueueDeclare(queue: "hello",
-                     durable: false,
+                     durable: true,
                      exclusive: false,
                      autoDelete: false,
                      arguments: null);
@@ -20,10 +20,15 @@ var message = GetMessage(args);
 
 var body = Encoding.UTF8.GetBytes(message);
 
+//marca as mensagens como persistentes, ficaram disponiveis mesmo quando o rabbit reiniciar
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
 channel.BasicPublish(exchange: string.Empty,
                      routingKey: "hello",
-                     basicProperties: null,
+                     basicProperties: properties,
                      body: body);
+
 Console.WriteLine($" [x] Sent {message}");
 
 Console.WriteLine(" Press [enter] to exit.");
